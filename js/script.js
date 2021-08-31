553bd4772e505f71097dfed9e7cbeeab04541f23
Art <zemlanskijartemij@gmail.com>
2021-08-31 14:48:29 +0300

    Timer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', nativeTabs);
-
-function nativeTabs() {
+window.addEventListener('DOMContentLoaded', function () {
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -34,6 +32,46 @@ function nativeTabs() {
                 }
             }
         }
+
     });
 
-}
+    // Timer
+
+    let deadline = '2021-10-01';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            'total': t,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours
+        };
+    }
+
+    function setClock(id, endtime){
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock(){
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if(t.total <= 0){
+                clearInterval(timeInterval);
+
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+});
